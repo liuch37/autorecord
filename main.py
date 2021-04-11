6aa6619944cfdbf8b97333c9d3b4e2926dd957c3
Liu Chun-Hao (CR/PJ-AI-S3) <lic1syv@bosch.com>
2021-04-11 01:20:57 -0700

    Enhance video recording

--- main.py
+++ main.py
@@ -8,38 +8,32 @@ import pdb
 
 def main():
     # get primary monitor screen size
-    img = pg.screenshot()
-    # convert these pixels to a proper numpy array to work with OpenCV
-    frame = np.array(img)
-    Height, Width, _ = frame.shape
-    SCREEN_SIZE = (Width, Height)
+    SCREEN_SIZE = tuple(pg.size())
     # frame per second
-    FPS = 10.0
+    FPS = 6.0
     # output video name and type
     output_name = "output.avi"
     # define codec
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
     # create the video write object
     out = cv2.VideoWriter(output_name, fourcc, FPS, SCREEN_SIZE)
-    print(SCREEN_SIZE)
-
+    print("Screen size:", SCREEN_SIZE)
+    print("Start recording......")
     while True:
-        # make a screenshot
-        img = pg.screenshot()
-        # convert these pixels to a proper numpy array to work with OpenCV
-        frame = np.array(img)
-        # convert colors from BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # write the frame
-        out.write(frame)
-        # show the frame
-        cv2.imshow("screenshot", frame)
-        # if the user clicks q, it exits
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        try:
+            # make a screenshot
+            img = pg.screenshot()
+            # convert these pixels to a proper numpy array to work with OpenCV
+            frame = np.array(img)
+            # convert colors from BGR to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # write the frame
+            out.write(frame)
+        except KeyboardInterrupt:
+            print("Stop recording......")
             break
 
     # make sure everything is closed when exited
-    cv2.destroyAllWindows()
     out.release()
 
 if __name__ == "__main__":
