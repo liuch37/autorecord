720dd46edb0ef5b5750bf2baa4be43ce3ebd651b
Chun-Hao Liu <chun-haoliu@chun-haos-air.lan>
2021-04-12 23:52:07 -0700

    Delay audio

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pyautogui as pg
 import ffmpeg
 import threading
 import msvcrt
+from pydub import AudioSegment
 from videorecord import ScreenRecorder
 from audiorecord import AudioRecorder
 
@@ -23,7 +24,9 @@ def main():
     # create screen recorder object
     vrec = ScreenRecorder(output_name=voutput_name, fps=FPS)
     # create audio recorder object
-    arec = AudioRecorder(output_name=aoutput_name, input_device_index=device_index, fps=FPS)
+    arec = AudioRecorder(
+        output_name=aoutput_name, input_device_index=device_index, fps=FPS
+    )
     # create video thread and start recording
     print("Start recording......")
     vrec.start()
@@ -38,19 +41,31 @@ def main():
     print("Merge video and audio......")
     timing_adjustment = vrec.elapsed_time - arec.elapsed_time
     if timing_adjustment < 0:
-        print("WARNING: Your recorded audio time is shorter than video time, which should not be correct. Check your frame rate for video.")
+        print(
+            "WARNING: Your recorded audio time is shorter than video time, which should not be correct. Check your frame rate for video."
+        )
     else:
         print("Adjust timing of audio by delaying (s)", timing_adjustment)
     # apply audio delay
-    #ffmpeg.exe -i "audio.wav" -itsoffset 10 -i "audio.wav" -map 1:a -c copy "audio-delayed.wav"
+    # create silent audio segment
+    silent_segment = AudioSegment.silent(
+        duration=int(timing_adjustment * 1000)
+    )  # duration in milliseconds
+    # read wav file to an audio segment
+    audio_segment = AudioSegment.from_wav(aoutput_name)
+    # add above two audio segments
+    delay_segment = silent_segment + audio_segment
+    # save modified audio
+    delay_segment.export(aoutput_name, format="wav")
 
     # merge audio and video
     input_video = ffmpeg.input(voutput_name)
     input_audio = ffmpeg.input(aoutput_name)
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output('merge.mp4').run()
+    ffmpeg.concat(input_video, input_audio, v=1, a=1).output("merge.mp4").run()
     # cmd line
-    #cmd = "ffmpeg -ac 2 -channel_layout stereo -i temp_audio.wav -i temp_video.avi -pix_fmt yuv420p " + filename + ".avi"
-    #subprocess.call(cmd, shell=True)
+    # cmd = "ffmpeg -ac 2 -channel_layout stereo -i temp_audio.wav -i temp_video.avi -pix_fmt yuv420p " + filename + ".avi"
+    # subprocess.call(cmd, shell=True)
+
 
 if __name__ == "__main__":
     main()
